35c430eea73f4f8c0cd0e76c0fd209fadc143049
Mads Jensen <mje.mads@gmail.com>
2016-06-11 11:32:14 +0200

    Added subjects list

--- my_settings.py
+++ my_settings.py
@@ -1,7 +1,5 @@
-
 # data_path = '/home/mje/Projects/agency_connectivity/Data'
 data_path = '/media/mje/My_Book/Data/agency_connectivity/'
-
 
 subjects_dir = data_path + "fs_subjects_dir/"
 save_folder = data_path + "filter_ica_data/"
@@ -11,4 +9,5 @@ tf_folder = data_path + "tf_data/"
 mne_folder = data_path + "minimum_norm/"
 log_folder = data_path + "log_files/"
 
-
+subjects = ["p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11",
+            "p12"]

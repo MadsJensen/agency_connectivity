de8f86bedf7d8cb2c0f69bd728651091f08dd8fe
Mads Jensen <au194693@wm.local>
2016-06-26 10:36:49 +0200

    Changes path and updated subjects

--- mne_preproc_wrapper.py
+++ mne_preproc_wrapper.py
@@ -4,15 +4,15 @@ Created on Fri May 20 20:50:00 2016
 
 @author: mje
 """
-
+from my_settings import *
 from preprocessing import *
 
-data_folder = '/media/mje/My_Book/Data/agency_connectivity/'
-
+# data_folder = '/media/mje/My_Book/Data/agency_connectivity/'
+data_folder = "/Volumes/My_Passport/agency_connectivity/Data/"
 
 # subjects = ["P2", "P3", "P4", "P5", "P6", "P7", "P8",
 #             "P10", "P11", "P12", "P13"]
-subjects = ["P13"]
+subjects = ["P6"]
 
 
 for subject in subjects:
--- my_settings.py
+++ my_settings.py
@@ -9,6 +9,5 @@ tf_folder = data_path + "tf_data/"
 mne_folder = data_path + "minimum_norm/"
 log_folder = data_path + "log_files/"
 
-#subjects = ["p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11",
-#            "p12"]
-subjects = ["P13", "P14", "P15", "P16", "P17", "P18", "P19"]
+subjects = ["p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11",
+            "p12", "p13", "P14", "P15", "P16", "P17", "P18", "P19"]

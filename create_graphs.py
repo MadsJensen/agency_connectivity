b8c7bd60f9e2089cb154def66a0585c9223c1f73
Mads Jensen <mje.mads@gmail.com>
2016-06-27 08:58:01 +0200

    fixes

---
+++ create_graphs.py
@@ -0,0 +1,57 @@
+import mne
+import sys
+import numpy as np
+from nitime import TimeSeries
+from nitime.analysis import CorrelationAnalyzer
+
+from my_settings import *
+
+bands = ["delta", "theta", "alpha", "beta", "gamma1", "gamma2"]
+
+times = np.arange(-2000, 2001, 1.95325)
+times = times / 1000.
+
+for subject in subjects:
+    ht_vol = np.load(tf_folder + "/%s_vol_HT-comp.npy" %
+                     subject)
+    ht_invol = np.load(tf_folder + "%s_inv_HT-comp.npy" %
+                       subject)
+
+    results_vol = {}
+    results_invol = {}
+
+    for j, band in enumerate(bands):
+        corr_vol = []
+        corr_invol = []
+
+        ht_vol_bs = mne.baseline.rescale(
+            np.abs(ht_vol[:, :, :, j])**2,
+            times,
+            baseline=(-2, -1.5),
+            mode="zscore")
+
+        ht_invol_bs = mne.baseline.rescale(
+            np.abs(ht_invol[:, :, :, j])**2,
+            times,
+            baseline=(-2, -1.5),
+            mode="zscore")
+
+        for ts in ht_vol_bs:
+            nits = TimeSeries(ts[:, 768:1024],
+                              sampling_rate=512)
+
+            corr_vol += [CorrelationAnalyzer(nits)]
+
+        for ts in ht_invol_bs:
+            nits = TimeSeries(ts[:, 768:1024],
+                              sampling_rate=512)
+
+            corr_invol += [CorrelationAnalyzer(nits)]
+
+        results_vol[band] = np.asarray([c.corrcoef for c in corr_vol])
+        results_invol[band] = np.asarray([c.corrcoef for c in corr_invol])
+
+    np.save(graph_data + "%s_vol_pow.npy" % subject,
+            results_vol)
+    np.save(graph_data + "%s_inv_pow.npy" % subject,
+            results_invol)
